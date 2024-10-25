45d0577c695dae6b59b4128d79228f9cc8c76416
Riley Karl <rileykarldesign@gmail.com>
2024-10-25 18:20:54 +0100

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,53 +14,53 @@ module.exports = {
 
     ],
   },
+  theme: {
+    extend: {
+      colors: {
+        'surface-1': "#f6f3ed",
+        'surface-2': "#ffffff",
+        'surface-3': "#4b4b4b05",
+        'type-1': "#403128",
+        'type-2': "#403128",
+        'stroke-1': "#796a59c6",
+        'stroke-2': "#403128",
+        'test': "#ffb300",
+      },
 
-  extend: {
-    colors: {
-      'surface-1': "#f6f3ed",
-      'surface-2': "#ffffff",
-      'surface-3': "#4b4b4b05",
-      'type-1': "#403128",
-      'type-2': "#403128",
-      'stroke-1': "#796a59ff",
-      'stroke-2': "#403128b8",
-      'test': "#ffb300",
-    },
+      width: {
+        'base': '423.875px',
+      },
+      spacing: {
+        '1': '15px',
+        '2': '25px',
+        '3': '30px',
+        '4': '40px',
+        '5': '50px',
+        '6': '60px',
+      },
 
-    width: {
-      'base': '423.875px',
-    },
-    spacing: {
-      '1': '15px',
-      '2': '25px',
-      '3': '30px',
-      '4': '40px',
-      '5': '50px',
-      '6': '60px',
-    },
+      fontFamily: {
+        'sans': ['altB'],
+        'serif': ['Times',],
+        'mono': ['ui-monospace'],
 
-    fontFamily: {
-      'sans': ['altB'],
-      'serif': ['Times',],
-      'mono': ['ui-monospace'],
-
-    },
-
-
-    borderRadius: {
-      'base': '3px'
-    },
-
-    height: {
-      'el': '30px'
-    },
+      },
 
 
-    aspectRatio: {
-      'main': '4 / 5',
+      borderRadius: {
+        'base': '3px'
+      },
+
+      height: {
+        'el': '30px'
+      },
+
+
+      aspectRatio: {
+        'main': '4 / 5',
+      },
     },
   },
-},
   plugins: [],
 }
 
